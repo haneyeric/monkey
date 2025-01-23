571ccf05f134dbf7244bd6f8bed8c47a85ceeeb4
Eric Haney <haneyeric@gmail.com>
2025-01-22 19:46:32 -0500

    Start Eval

---
+++ evaluator/evaluator.go
@@ -0,0 +1,44 @@
+package evaluator
+
+import (
+	"github.com/haneyeric/monkey/ast"
+	"github.com/haneyeric/monkey/object"
+)
+
+var (
+	NULL   = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+func nativeBoolToBooleanOjbect(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
